31e5fd066af87cbab585ad91022df88ca8289bd2
Átila <144128328+atilaF03@users.noreply.github.com>
2025-02-20 13:56:40 +0000

    atualizar

--- Arraylist2.java
+++ Arraylist2.java
@@ -1,25 +1,44 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Arraylist2 {
   public static void main(String[] args) {
-    Scanner sc =new Scanner(System.in);
-    ArrayList<String> nomes  = new ArrayList<>();
-         ArrayList<String> telefone  = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> nomes = new ArrayList<>();
+    ArrayList<String> telefones = new ArrayList<>();
 
-     
-     
-     
+    while (true) {
+      informacoes();
+      int opcao = sc.nextInt();
+      switch (opcao) {
+        case 1:
+          System.out.println("informe um nome");
+          String nome = sc.next();
+          System.out.println("informe um telefone");
+          String telefone = sc.nextLine();
+          nomes.add(nome);
+          telefones.add(telefone);
+          break;
+        case 2:
+        
+        
+        
+        break;
 
-
-
-
-
-
-
-
+        default:
+          break;
+      }
 
     }
-  }  
 
+  }
+
+  public static void informacoes() {
+    System.out.println("++++menu++++");
+    System.out.println("1- cadastrar contatos");
+    System.out.println("2-Listar Contatos");
+    System.out.println("3-Excluir ");
+    System.out.println("4- sair ");
+
+  }
+}
